197e90e8d56a41605bdb3a8f3e51271650b0d7be
Kiri Choi <kirichoi@uw.edu>
2019-02-15 16:28:08 -0800

    Update example model

--- ioutils.py
+++ ioutils.py
@@ -117,10 +117,10 @@ def testModels(modelType):
        
         S0 = 4
         S5 = 5
-        S1 = 0
-        S2 = 0
-        S3 = 0
-        S4 = 0
+        S1 = 1
+        S2 = 1
+        S3 = 1
+        S4 = 1
         """
     elif modelType == 'Linear_i':
         # Linear    
